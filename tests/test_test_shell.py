55ec7bc49c7ff1e58e78290920960392e710086b
hyomin <dksgyals1@gmail.com>
2025-08-05 17:16:37 +0900

    fix: resolve confilcts
    # Conflicts:
    #	tests/test_test_shell.py

--- tests/test_test_shell.py
+++ tests/test_test_shell.py
@@ -1,7 +1,11 @@
 import pytest
-
+from pytest_mock import MockerFixture
 from test_shell import TestShellApp
 
+@pytest.fixture
+def shell_app():
+    test_shell_app = TestShellApp()
+    return test_shell_app
 
 def test_shell_write():
     pass
@@ -43,24 +47,50 @@ def test_shell_read_after_read():
     pass
 
 
-def test_shell_wrong_cmd():
-    pass
+@pytest.mark.parametrize("input", ["exi", "rea", "wri", "hel"])
+def test_shell_wrong_cmd(shell_app, mocker: MockerFixture, input, capsys):
+    mocker.patch("builtins.input", side_effect=[input])
+
+    shell_app.run(1)
+    captured = capsys.readouterr()
+
+    assert 'INVALID COMMAND' in captured.out
 
 
-def test_shell_wrong_cmd_format():
-    pass
+@pytest.mark.skip
+def test_shell_wrong_cmd_empty(shell_app, mocker: MockerFixture, capsys):
+    mocker.patch("builtins.input", side_effect=[""])
+
+    shell_app.run(1)
+    captured = capsys.readouterr()
+
+    assert 'INVALID COMMAND' in captured.out
 
 
-def test_shell_wrong_cmd_args():
-    pass
+@pytest.mark.parametrize("input", ["read 1 2", "write", "fullwrite", "exit 3"])
+def test_shell_wrong_cmd_args(shell_app, mocker: MockerFixture, input, capsys):
+    mocker.patch("builtins.input", side_effect=["read 1 2"])
 
+    shell_app.run(1)
+    captured = capsys.readouterr()
+
+    assert 'INVALID COMMAND' in captured.out
 
 def test_shell_subprocess_cmd():
     pass
 
 
-def test_shell_exit():
-    pass
+# @pytest.mark.skip
+def test_shell_exit(shell_app, mocker: MockerFixture):
+    mocker.patch("builtins.input", side_effect=["exit"])
+    exit_mock = mocker.patch("test_shell.TestShellApp.exit", side_effect=SystemExit(0))
+
+    with pytest.raises(SystemExit) as e:
+        shell_app.run(1)
+
+    exit_mock.assert_called_once()
+    assert e.value.code == 0
+
 
 
 def test_shell_help(capsys):
