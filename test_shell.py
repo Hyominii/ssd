e7681eb913dc7e5782ffe60b590d13d696f876b7
dhoon21 <ijdmc2002@naver.com>
2025-08-05 16:06:03 +0900

    [feature] add help function
    Help function shows 팀명, 팀장, 팀원, 명령어 사용법
    
    Signed-off-by: dhoon21 <ijdmc2002@naver.com>

--- test_shell.py
+++ test_shell.py
@@ -15,7 +15,15 @@ class TestShellApp:
         pass
 
     def help(self):
-        pass
+        print("팀명: BestReviewer")
+        print("팀장: 이장희 / 팀원: 김대용, 최도현, 박윤상, 최동희, 안효민, 김동훈")
+        print("사용 가능한 명령어:")
+        print("  write <LBA> <Value>      : 특정 LBA에 값 저장")
+        print("  read <LBA>               : 특정 LBA 값 읽기")
+        print("  fullwrite <Value>        : 전체 LBA에 동일 값 저장")
+        print("  fullread                 : 전체 LBA 읽기 및 출력")
+        print("  help                     : 도움말 출력")
+        print("  exit                     : 종료")
 
     def run(self):
         pass
--- tests/test_test_shell.py
+++ tests/test_test_shell.py
@@ -1,5 +1,7 @@
 import pytest
 
+from ssd.test_shell import TestShellApp
+
 
 def test_shell_write():
     pass
@@ -61,9 +63,27 @@ def test_shell_exit():
     pass
 
 
-def test_shell_help():
-    pass
+def test_shell_help(capsys):
+    test_shell_app = TestShellApp()
+    test_shell_app.help()
 
+    captured = capsys.readouterr()
+    output_lines = captured.out.strip().splitlines()
+
+    expected_lines = [
+        "팀명: BestReviewer",
+        "팀장: 이장희 / 팀원: 김대용, 최도현, 박윤상, 최동희, 안효민, 김동훈",
+        "사용 가능한 명령어:",
+        "  write <LBA> <Value>      : 특정 LBA에 값 저장",
+        "  read <LBA>               : 특정 LBA 값 읽기",
+        "  fullwrite <Value>        : 전체 LBA에 동일 값 저장",
+        "  fullread                 : 전체 LBA 읽기 및 출력",
+        "  help                     : 도움말 출력",
+        "  exit                     : 종료",
+    ]
+
+    for line in expected_lines:
+        assert line in output_lines
 
 def test_shell_full_read():
     pass
