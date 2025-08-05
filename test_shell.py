1979ae3ffd13f81a254b90a3b2b12bb02fec0d1d
dhoon21 <ijdmc2002@naver.com>
2025-08-05 16:24:57 +0900

    [refactoring] Change hard coding to using variable
    Signed-off-by: dhoon21 <ijdmc2002@naver.com>

--- test_shell.py
+++ test_shell.py
@@ -15,8 +15,11 @@ class TestShellApp:
         pass
 
     def help(self):
-        print("팀명: BestReviewer")
-        print("팀장: 이장희 / 팀원: 김대용, 최도현, 박윤상, 최동희, 안효민, 김동훈")
+        team_name = "BestReviewer"
+        team_leader = "이장희"
+        team_members = ["김대용", "최도현", "박윤상", "최동희", "안효민", "김동훈"]
+        print(f"팀명: {team_name}")
+        print(f"팀장: {team_leader} / 팀원: {", ".join(team_members)}")
         print("사용 가능한 명령어:")
         print("  write <LBA> <Value>      : 특정 LBA에 값 저장")
         print("  read <LBA>               : 특정 LBA 값 읽기")
@@ -32,3 +35,4 @@ class TestShellApp:
 if __name__ == "__main__":
     app = TestShellApp()
     app.run()
+    app.help()
