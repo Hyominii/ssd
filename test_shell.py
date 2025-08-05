8c1331fdbedccafe6a69fd7010ffb5a129090fd5
hyomin <dksgyals1@gmail.com>
2025-08-05 17:16:37 +0900

    fix: resolve confilcts
    # Conflicts:
    #	tests/test_test_shell.py

--- test_shell.py
+++ test_shell.py
@@ -1,3 +1,6 @@
+import shlex
+
+
 class TestShellApp:
     def read(self, address: int):
         pass
@@ -12,7 +15,7 @@ class TestShellApp:
         pass
 
     def exit(self):
-        pass
+        exit(0)
 
     def help(self):
         print("팀명: BestReviewer")
@@ -25,8 +28,55 @@ class TestShellApp:
         print("  help                     : 도움말 출력")
         print("  exit                     : 종료")
 
-    def run(self):
-        pass
+    def run(self, max_iterations: int = None):
+        print(f"안녕하세요, SSD 검증용 Test Shell App을 시작합니다.\n")
+
+        while True:
+            if max_iterations is not None:
+                if max_iterations <= 0:
+                    break
+                max_iterations -= 1
+            command = input("Shell > ").strip()
+            parts = shlex.split(command)  # 공백을 기준으로 파싱하되 인용된 문자열도 처리
+            cmd_name, *cmd_args = parts
+            if self.is_validate_command(cmd_name, cmd_args):
+                if cmd_name == "exit":
+                    self.exit()
+                elif cmd_name == "help":
+                    self.help()
+                elif cmd_name == "write":
+                    self.write(cmd_args[0], cmd_args[1])
+                elif cmd_name == "read":
+                    self.read(cmd_args[0])
+                elif cmd_name == "fullwrite":
+                    self.full_write(cmd_args[0])
+                elif cmd_name == "fullread":
+                    self.full_read()
+                else:
+                    self.print_invalid_command()
+
+    def is_validate_command(self, cmd_name, cmd_args):
+        if cmd_name == "exit" or cmd_name == "help" or cmd_name == "fullread":
+            if len(cmd_args) > 0:
+                self.print_invalid_command()
+                return False
+        elif cmd_name == "write":
+            if len(cmd_args) != 2:
+                self.print_invalid_command()
+                return False
+        elif cmd_name == "read":
+            if len(cmd_args) != 1:
+                self.print_invalid_command()
+                return False
+        elif cmd_name == "fullwrite":
+            if len(cmd_args) != 1:
+                self.print_invalid_command()
+                return False
+
+        return True
+
+    def print_invalid_command(self):
+        print("INVALID COMMAND")
 
 
 if __name__ == "__main__":
