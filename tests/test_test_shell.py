a1aba32de86ee2fc558d1a7b5a4c628a2999af8b
hyomin <dksgyals1@gmail.com>
2025-08-05 17:24:36 +0900

    fix: empty commmand case
    # Conflicts:
    #	tests/test_test_shell.py

--- tests/test_test_shell.py
+++ tests/test_test_shell.py
@@ -57,7 +57,7 @@ def test_shell_wrong_cmd(shell_app, mocker: MockerFixture, input, capsys):
     assert 'INVALID COMMAND' in captured.out
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_shell_wrong_cmd_empty(shell_app, mocker: MockerFixture, capsys):
     mocker.patch("builtins.input", side_effect=[""])
 
