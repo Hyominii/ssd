27f7e91077f52fe0a3340407c3334d0b569b81cc
donghee <do.choi@samsung.com>
2025-08-06 09:04:22 +0900

    [feature] fix read function

--- ssd.py
+++ ssd.py
@@ -3,7 +3,7 @@ import os
 
 OUTPUT_FILE = 'ssd_output.txt'
 TARGET_FILE = 'ssd_nand.txt'
-
+BLANK_STRING = "0x00000000"
 ERROR_STRING = 'ERROR'
 
 class SSD:
@@ -13,12 +13,11 @@ class SSD:
         pass
 
     def init_file(self, filename: str):
-        # BLANK_VALUE = "0x00000000"
         # 파일이 없으면 새로 생성
-        # 100칸이 있어야 하므로 100개의 BLANCK VALUE 생성
+        # 100칸이 있어야 하므로 100개의 BLANK VALUE 생성
         if not os.path.exists(filename):
             with open(filename, "w") as f:
-                [f.write("0x00000000"+"\n") for _ in range(100)]
+                [f.write(BLANK_STRING + "\n") for _ in range(100)]
         return
 
     def read(self, address: int) -> int:
@@ -27,10 +26,11 @@ class SSD:
             lines = f.readlines()
 
         if len(lines) >= address:
-            return lines[address].rstrip("\n")  # 0-based index
+            with open(OUTPUT_FILE, "w") as f:
+                f.write(lines[address].rstrip("\n"))
         else:
             with open(OUTPUT_FILE, "w") as f:
-                f.write(ERROR_STRING + "\n")
+                f.write(ERROR_STRING)
         return 0
 
 
