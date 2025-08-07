d7b303b3d11195be0c35a7d6e2f0c9d5f0fd5e99
Janghee.Lee <lee.janghee@gmail.com>
2025-08-07 11:04:11 +0900

    [Refactor] Command Pattern 추가

---
+++ command.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+# 추상 Command 클래스
+class Command(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+    # 구체적인 Command들
+
+
+class CommandInvoker:
+    def __init__(self):
+        self._commands = []
+
+    def add_command(self, cmd: Command):
+        self._commands.append(cmd)
+
+    def flush(self):
+        for cmd in self._commands:
+            cmd.execute()
+        self._commands.clear()
