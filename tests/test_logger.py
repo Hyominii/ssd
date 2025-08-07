ac3ac7598b742e9f403a68a65c038f21b7e9a34d
hyomin <dksgyals1@gmail.com>
2025-08-07 13:18:16 +0900

    test: add file handler test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,72 +1,77 @@
 import sys
 
 import pytest
-from logger import Logger, StreamHandler
+from logger import Logger, StreamHandler, FileHandler
+
+CLASS_METHOD_NAME = "class.method()"
+MESSAGE = "Hi"
 
 
 @pytest.fixture
 def logger():
     logger = Logger()
-    logger.clear() # clear handlers
+    logger.clear()  # clear handlers
 
     return logger
 
 
 def test_logger_print_success(logger, capsys):
-    class_method_name = "class.method()"
-    message = "Hi"
     logger.add_handler(StreamHandler(sys.stdout))
-    logger.print(class_method_name, message)
+    logger.print(CLASS_METHOD_NAME, MESSAGE)
 
     captured = capsys.readouterr()
     print(f"captured.out: {captured.out}")
 
-    assert f"{class_method_name}: {message}" in captured.out
+    assert f"{CLASS_METHOD_NAME}: {MESSAGE}" in captured.out
 
 
 def test_logger_info_success(logger, capsys):
-    class_method_name = "class.method()"
-    message = "Hi"
     logger.add_handler(StreamHandler(sys.stdout))
-    logger.info(class_method_name, message)
+    logger.info(CLASS_METHOD_NAME, MESSAGE)
 
     captured = capsys.readouterr()
     print(f"captured.out: {captured.out}")
 
-    assert f"INFO {class_method_name}: {message}" in captured.out
+    assert f"INFO {CLASS_METHOD_NAME}: {MESSAGE}" in captured.out
 
 
 def test_logger_warn_success(logger, capsys):
-    class_method_name = "class.method()"
-    message = "Hi"
     logger.add_handler(StreamHandler(sys.stdout))
-    logger.warn(class_method_name, message)
+    logger.warn(CLASS_METHOD_NAME, MESSAGE)
 
     captured = capsys.readouterr()
     print(f"captured.out: {captured.out}")
 
-    assert f"WARN {class_method_name}: {message}" in captured.out
+    assert f"WARN {CLASS_METHOD_NAME}: {MESSAGE}" in captured.out
 
 
 def test_logger_error_success(logger, capsys):
-    class_method_name = "class.method()"
-    message = "Hi"
     logger.add_handler(StreamHandler(sys.stdout))
-    logger.error(class_method_name, message)
+    logger.error(CLASS_METHOD_NAME, MESSAGE)
 
     captured = capsys.readouterr()
     print(f"captured.out: {captured.out}")
 
-    assert f"ERROR {class_method_name}: {message}" in captured.out
+    assert f"ERROR {CLASS_METHOD_NAME}: {MESSAGE}" in captured.out
 
 
 def test_logger_debug_success(logger, capsys):
-    class_method_name = "class.method()"
-    message = "Hi"
     logger.add_handler(StreamHandler(sys.stdout))
-    logger.debug(class_method_name, message)
+    logger.debug(CLASS_METHOD_NAME, MESSAGE)
 
     captured = capsys.readouterr()
     print(f"captured.out: {captured.out}")
 
-    assert f"DEBUG {class_method_name}: {message}" in captured.out
+    assert f"DEBUG {CLASS_METHOD_NAME}: {MESSAGE}" in captured.out
+
+
+def test_file_handler_writes_to_file(tmp_path, logger):
+    log_path = tmp_path / "test.log"
+
+    handler = FileHandler(dirname=tmp_path, filename="test.log")  # 1KB 용량 제한
+    logger.add_handler(handler)
+    logger.print(CLASS_METHOD_NAME, MESSAGE)
+
+    content = log_path.read_text()
+    assert log_path.exists()
+    assert "class.method(): Hi" in content
